c51ea3a5c7861bc2e44fc3a17889bd01045dc41e
krosh22 <Olga2018r@yandex.ru>
2021-02-05 18:43:51 +0300

    fix double

--- js/js.js
+++ js/js.js
@@ -7,4 +7,16 @@
             header.classList.remove("header_activ");
             }
     }
-}())
+}());
+
+(function () {
+    const burgerItem = document.querySelector(".burger");
+    const menu = document.querySelector(".header_now");
+    const menuCloseItem = document.querySelector(".header_nav-close");
+    burgerItem.addEventListener("click", () => {
+        menu.classList.add(".header_now_active");
+    });
+    menuCloseItem.addEventListener("click", () => {
+        menu.classList.remove("header_now_active");
+    });
+    }());
